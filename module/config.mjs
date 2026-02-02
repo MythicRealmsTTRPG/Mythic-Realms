6bcea285d1313550be3a787cd457ceef939ec65c
ScaleScribe <scalescribe.mythicrealms@gmail.com>
2026-02-01 20:46:33 -0600

    ASCII

---
+++ module/config.mjs
@@ -0,0 +1,45 @@
+/**
+ * @import {
+ *   AbilityConfiguration, ActivityActivationTypeConfiguration, ActivityConsumptionTargetConfiguration,
+ *   ActivityTypeConfiguration, ActorSizeConfiguration, AdvancementTypeConfiguration,
+ *   AreaTargetDefinition, CalendarHUDConfiguration, CharacterFlagConfiguration, ConditionConfiguration,
+ *   CraftingConfiguration, CreatureTypeConfiguration, CurrencyConfiguration, DamageTypeConfiguration,
+ *   EncumbranceConfiguration, FacilityConfiguration, HabitatConfiguration5e,
+ *   IndividualTargetDefinition, ItemPropertyConfiguration, LimitedUsePeriodConfiguration,
+ *   MapLocationMarkerStyle, MovementTypeConfiguration, MovementUnitConfiguration,
+ *   RestTypeConfiguration, RequestCallback5e, RuleTypeConfiguration, SkillConfiguration,
+ *   SpellcastingFocusConfiguration, SpellcastingPreparationState5e, SpellSchoolConfiguration,
+ *   SpellScrollValues, StatusEffectConfig5e, SubtypeTypeConfiguration, TimeUnitConfiguration,
+ *   ToolConfiguration, TraitConfiguration, TransformationConfiguration, TravelPaceConfiguration,
+ *   TravelUnitConfiguration, TreasureConfiguration5e, UnitConfiguration, WeaponMasterConfiguration
+ * } from "./_types.mjs";
+ * @import { TravelPace5e } from "./data/actor/fields/_types.mjs";
+ * @import {
+ *   MultiLevelSpellcasting, SingleLevelSpellcastingData, SlotSpellcastingData, SpellcastingModelData,
+ *   SpellcastingTable5e, SpellcastingTableSingle5e
+ * } from "./data/spellcasting/_types.mjs";
+ */
+
+// Namespace Configuration Values
+const MYTHICREALMS = {};
+
+//ASCII Artwork
+MYTHICREALMS.ASCII = `__________________________________________________
+
+ ███╗   ███╗██╗   ██╗████████╗██╗  ██╗██╗ ██████╗
+  ████╗ ████║╚██╗ ██╔╝╚══██╔══╝██║  ██║██║██╔════╝
+  ██╔████╔██║ ╚████╔╝    ██║   ███████║██║██║
+  ██║╚██╔╝██║  ╚██╔╝     ██║   ██╔══██║██║██║
+  ██║ ╚═╝ ██║   ██║      ██║   ██║  ██║██║╚██████╗
+  ╚═╝     ╚═╝   ╚═╝      ╚═╝   ╚═╝  ╚═╝╚═╝ ╚═════╝
+
+             
+
+              ██████╗ ███████╗ █████╗ ██╗     ███╗   ███╗███████╗
+              ██╔══██╗██╔════╝██╔══██╗██║     ████╗ ████║██╔════╝
+              ██████╔╝█████╗  ███████║██║     ██╔████╔██║███████╗
+              ██╔══██╗██╔══╝  ██╔══██║██║     ██║╚██╔╝██║╚════██║
+              ██║  ██║███████╗██║  ██║███████╗██║ ╚═╝ ██║███████║
+              ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝╚══════╝___________`;
+
+
